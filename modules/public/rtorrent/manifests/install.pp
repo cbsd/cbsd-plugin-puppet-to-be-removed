aa7518f2961ca29b769c064d946e43d5b030dea6
Charlie Root <root@puppetserver.my.domain>
2020-06-03 18:15:17 +0300

    wip

--- modules/public/rtorrent/manifests/install.pp
+++ modules/public/rtorrent/manifests/install.pp
@@ -2,7 +2,7 @@ class rtorrent::install {
 
 #    class { 'php': }
 
-    $packages = [ "sysutils/tmux", "devel/git", "security/ca_root_nss", "archivers/unrar", "archivers/rar", "archivers/zip", "multimedia/mediainfo", "multimedia/ffmpeg", "ftp/curl" ]
+    $packages = [ "sysutils/tmux", "devel/git", "security/ca_root_nss", "archivers/unrar", "archivers/rar", "archivers/zip", "multimedia/mediainfo", "multimedia/ffmpeg", "ftp/curl", "audio/sox", "lang/python3", "net/py-cloudscraper" ]
 
     package { $::rtorrent::package_name:
         ensure => $::rtorrent::package_ensure,
