8b39f5c2f52f02dc7629bcda047a738d19a94a07
Charlie Root <root@gizmo.my.domain>
2020-06-03 23:31:34 +0300

    wip

--- modules/public/rtorrent/manifests/users.pp
+++ modules/public/rtorrent/manifests/users.pp
@@ -1,15 +1,15 @@
 class rtorrent::users {
 
-    group { "$::rtorrent::group":
-        ensure     => present,
-        forcelocal => true,
-    }
+  group { "$::rtorrent::group":
+    ensure     => present,
+    forcelocal => true,
+  }
 
-    user { "$::rtorrent::user":
-        ensure => present,
-        forcelocal => true,
-        groups     => [ "$::rtorrent::group" ],
-        shell      => '/bin/sh',
-        home       => "/usr/home/web",
-    }
+  user { "$::rtorrent::user":
+    ensure => present,
+    forcelocal => true,
+    groups     => [ "$::rtorrent::group" ],
+    shell      => '/bin/sh',
+    home       => "/usr/home/web",
+  }
 }
